656bf3f54b70e0777f01a835a147f9ed1eb9682f
M0vi <a>
2026-02-08 22:41:13 -0300

    update route.ts

--- app/script/route.ts
+++ app/script/route.ts
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic'
 
 export async function GET() {
 
-  const luaLoader = `loadstring(request({Url="https://michigun.xyz/script",Method="GET"}).Body)()`
+  const luaLoader = `loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/66b35878a8bf3053747f543e17f7cdd565caa7d0bf5712a768ce5a874eb74c9e/download"))()`
   
   return new NextResponse(luaLoader, {
     status: 200,
