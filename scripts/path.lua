0032d940eacf2a4ffa251eafb54f2141dc2b1d82
M0vi <a>
2026-02-15 16:02:31 -0300

    update

--- scripts/path.lua
+++ scripts/path.lua
@@ -1,16 +1,16 @@
 -- [[ sai daqui cara :sob:]]
 return {
-    silent = "silent.lua.txt",
-    hitbox = "hitbox.lua.txt",
-    esp = "esp.lua.txt",
-    tas = "tas.lua.txt",
-    jj = "jj.lua.txt",
-    f3x = "f3x.lua.txt",
-    char = "char.lua.txt",
-    antilag = "antilag.lua.txt",
-    chatgpt = "chatgpt.lua.txt",
-    player = "player.lua.txt",
-    tevez = "tevez.lua.txt",
-    delta = "delta.lua.txt",
-    soucre = "soucre.lua.txt"
+    silent = "silent.lua",
+    hitbox = "hitbox.lua",
+    esp = "esp.lua",
+    tas = "tas.lua",
+    jj = "jj.lua",
+    f3x = "f3x.lua",
+    char = "char.lua",
+    antilag = "antilag.lua",
+    chatgpt = "chatgpt.lua",
+    player = "player.lua",
+    tevez = "tevez.lua",
+    delta = "delta.lua",
+    soucre = "soucre.lua"
 }
