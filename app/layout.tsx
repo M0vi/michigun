2e54ced32695b5090f1c2ceed8ed4283fce19ab3
M0vi <you@example.com>
2026-03-22 00:44:15 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,15 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://michigun.xyz'),
   title: 'michigun.xyz',
   description:
-    'Script com diversas funções para jogos de Exército Brasileiro',
+    'Script para Exército Brasileiro no Roblox',
   openGraph: {
     title: 'michigun.xyz',
-    description: 'Script para Exército Brasileiro no Roblox.',
+    description: 'Script para Exército Brasileiro no Roblox',
     url: 'https://michigun.xyz',
     siteName: 'michigun.xyz',
     images: [
       {
-        url: '/avatar.png'
+        url: '/avatar.png',
         width: 1200,
         height: 630,
         alt: 'michigun.xyz',
